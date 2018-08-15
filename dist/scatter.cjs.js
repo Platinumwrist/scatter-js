b238fce635ee79cc9752fe4c8879fefd6e30f980
Liam Curry <liam@sagan.software>
2018-08-15 17:31:16 -0400

    removed eth

--- dist/scatter.cjs.js
+++ dist/scatter.cjs.js
@@ -5,11 +5,6 @@ function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'defau
 var io = _interopDefault(require('socket.io-client'));
 var getRandomValues = _interopDefault(require('get-random-values'));
 var Eos = _interopDefault(require('eosjs'));
-var ProviderEngine = _interopDefault(require('web3-provider-engine'));
-var RpcSubprovider = _interopDefault(require('web3-provider-engine/subproviders/rpc'));
-var WebsocketSubprovider = _interopDefault(require('web3-provider-engine/subproviders/websocket'));
-var HookedWalletSubprovider = _interopDefault(require('web3-provider-engine/subproviders/hooked-wallet'));
-var ethUtil = _interopDefault(require('ethereumjs-util'));
 require('isomorphic-fetch');
 
 class StorageService {
@@ -351,87 +346,7 @@ class EOS extends Plugin {
     }
 }
 
-let ethNetwork;
-
-class ETH extends Plugin {
-
-    constructor(){
-        super(Blockchains.ETH, BLOCKCHAIN_SUPPORT);
-    }
-
-    signatureProvider(...args){
-
-        return (_network, _web3) => {
-            ethNetwork = Network.fromJson(_network);
-            if(!ethNetwork.isValid()) throw Error.noNetwork();
-
-            const rpcUrl = `${ethNetwork.protocol}://${ethNetwork.hostport()}`;
-
-            const engine = new ProviderEngine();
-            const web3 = new _web3(engine);
-
-            const walletSubprovider = new HookedWalletSubprovider(new ScatterEthereumWallet());
-            engine.addProvider(walletSubprovider);
-
-            if(ethNetwork.protocol.indexOf('http') > -1) engine.addProvider(new RpcSubprovider({rpcUrl}));
-            else engine.addProvider(new WebsocketSubprovider({rpcUrl}));
-
-            engine.start();
-
-            return web3;
-        }
-    }
-}
-
-
-
-class ScatterEthereumWallet {
-    constructor(){
-        this.getAccounts = this.getAccounts.bind(this);
-        this.signTransaction = this.signTransaction.bind(this);
-    }
-
-    async getAccounts(callback) {
-        const result = await SocketService.sendApiRequest({
-            type:'identityFromPermissions',
-            payload:{}
-        });
-        const accounts = !result ? [] : result.accounts
-            .filter(account => account.blockchain === Blockchains.ETH)
-            .map(account => account.address);
-
-        callback(null, accounts);
-        return accounts;
-    }
-
-    async signTransaction(transaction){
-        if(!ethNetwork) throw Error.noNetwork();
-
-        // Basic settings
-        if (transaction.gas !== undefined) transaction.gasLimit = transaction.gas;
-        transaction.value = transaction.value || '0x00';
-        if(transaction.hasOwnProperty('data')) transaction.data = ethUtil.addHexPrefix(transaction.data);
-
-        // Required Fields
-        const requiredFields = transaction.hasOwnProperty('requiredFields') ? transaction.requiredFields : {};
-
-        // Contract ABI
-        const abi = transaction.hasOwnProperty('abi') ? transaction.abi : null;
-        if(!abi && transaction.hasOwnProperty('data'))
-            throw Error.signatureError('no_abi', 'You must provide a JSON ABI along with your transaction so that users can read the contract');
-
-        const payload = Object.assign(transaction, { blockchain:Blockchains.ETH, network:ethNetwork, requiredFields });
-        const {signatures, returnedFields} = await SocketService.sendApiRequest({
-            type:'requestSignature',
-            payload
-        });
-
-        if(transaction.hasOwnProperty('fieldsCallback'))
-            transaction.fieldsCallback(returnedFields);
-
-        return signatures[0];
-    }
-}
+// import ETH from './defaults/eth';
 
 /***
  * Setting up for plugin based generators,
@@ -447,7 +362,7 @@ class PluginRepositorySingleton {
 
     loadPlugins(){
         this.plugins.push(new EOS());
-        this.plugins.push(new ETH());
+        // this.plugins.push(new ETH());
     }
 
     signatureProviders(){
--- dist/scatter.esm.js
+++ dist/scatter.esm.js
@@ -1,11 +1,6 @@
 import io from 'socket.io-client';
 import getRandomValues from 'get-random-values';
 import Eos from 'eosjs';
-import ProviderEngine from 'web3-provider-engine';
-import RpcSubprovider from 'web3-provider-engine/subproviders/rpc';
-import WebsocketSubprovider from 'web3-provider-engine/subproviders/websocket';
-import HookedWalletSubprovider from 'web3-provider-engine/subproviders/hooked-wallet';
-import ethUtil from 'ethereumjs-util';
 import 'isomorphic-fetch';
 
 class StorageService {
@@ -347,87 +342,7 @@ class EOS extends Plugin {
     }
 }
 
-let ethNetwork;
-
-class ETH extends Plugin {
-
-    constructor(){
-        super(Blockchains.ETH, BLOCKCHAIN_SUPPORT);
-    }
-
-    signatureProvider(...args){
-
-        return (_network, _web3) => {
-            ethNetwork = Network.fromJson(_network);
-            if(!ethNetwork.isValid()) throw Error.noNetwork();
-
-            const rpcUrl = `${ethNetwork.protocol}://${ethNetwork.hostport()}`;
-
-            const engine = new ProviderEngine();
-            const web3 = new _web3(engine);
-
-            const walletSubprovider = new HookedWalletSubprovider(new ScatterEthereumWallet());
-            engine.addProvider(walletSubprovider);
-
-            if(ethNetwork.protocol.indexOf('http') > -1) engine.addProvider(new RpcSubprovider({rpcUrl}));
-            else engine.addProvider(new WebsocketSubprovider({rpcUrl}));
-
-            engine.start();
-
-            return web3;
-        }
-    }
-}
-
-
-
-class ScatterEthereumWallet {
-    constructor(){
-        this.getAccounts = this.getAccounts.bind(this);
-        this.signTransaction = this.signTransaction.bind(this);
-    }
-
-    async getAccounts(callback) {
-        const result = await SocketService.sendApiRequest({
-            type:'identityFromPermissions',
-            payload:{}
-        });
-        const accounts = !result ? [] : result.accounts
-            .filter(account => account.blockchain === Blockchains.ETH)
-            .map(account => account.address);
-
-        callback(null, accounts);
-        return accounts;
-    }
-
-    async signTransaction(transaction){
-        if(!ethNetwork) throw Error.noNetwork();
-
-        // Basic settings
-        if (transaction.gas !== undefined) transaction.gasLimit = transaction.gas;
-        transaction.value = transaction.value || '0x00';
-        if(transaction.hasOwnProperty('data')) transaction.data = ethUtil.addHexPrefix(transaction.data);
-
-        // Required Fields
-        const requiredFields = transaction.hasOwnProperty('requiredFields') ? transaction.requiredFields : {};
-
-        // Contract ABI
-        const abi = transaction.hasOwnProperty('abi') ? transaction.abi : null;
-        if(!abi && transaction.hasOwnProperty('data'))
-            throw Error.signatureError('no_abi', 'You must provide a JSON ABI along with your transaction so that users can read the contract');
-
-        const payload = Object.assign(transaction, { blockchain:Blockchains.ETH, network:ethNetwork, requiredFields });
-        const {signatures, returnedFields} = await SocketService.sendApiRequest({
-            type:'requestSignature',
-            payload
-        });
-
-        if(transaction.hasOwnProperty('fieldsCallback'))
-            transaction.fieldsCallback(returnedFields);
-
-        return signatures[0];
-    }
-}
+// import ETH from './defaults/eth';
 
 /***
  * Setting up for plugin based generators,
@@ -443,7 +358,7 @@ class PluginRepositorySingleton {
 
     loadPlugins(){
         this.plugins.push(new EOS());
-        this.plugins.push(new ETH());
+        // this.plugins.push(new ETH());
     }
 
     signatureProviders(){
